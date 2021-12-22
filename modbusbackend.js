1cd03447bacb84f8e8c1103d2d891cb8e00ce49e
Beke András <beke.andras.mkolc@gmail.com>
2021-12-22 15:37:00 +0100

    Cleaning.

--- modbusbackend.js
+++ modbusbackend.js
@@ -1,6 +1,4 @@
 
-
-
 var express = require('express');
 var app = express();
 var PORT = 3000;
@@ -16,9 +14,16 @@ SerialPort.list().then(
 
 const port = new SerialPort('COM2')
 
-/* Outbuffer to serial port */
+/**
+ * Max outbuffer size.
+ */
 
-const outBuffer = Buffer.from([0x01, 0x03, 0x00, 0x00, 0x00, 0x0A, 0xC5, 0xCD]);
+const MODBUS_OUT_BUFFER_SIZE = 50;
+/** 
+ * Outbuffer to serial port 
+ * */
+
+const outBuffer = Buffer.alloc(MODBUS_OUT_BUFFER_SIZE);
 
 // Compute the MODBUS RTU CRC
 function ModRTU_CRC(buf, len) {
@@ -40,65 +45,70 @@ function ModRTU_CRC(buf, len) {
     return parseInt(crc);
 }
 /* MODBUF function codes. We use the 03, and 06 fiunctions now. The 03 function's register count is one for simplicity. */
-const READ_COILS                = 0x01;
-const READ_DISCRETE_INPUTS      = 0x02;
-const READ_HOLDING_REGISTERS    = 0x03;
-const READ_INPUT_REGISTERS      = 0x04;
-const WRITE_SINGLE_COIL         = 0x05; 
-const WRITE_SINGLE_REGISTER     = 0x06;
-const READ_EXCEPTION_STATUS     = 0x07;
-const GET_COMM_EVENT_COUNTER    = 0x0B;
-const GET_COMM_EVENT_LOG        = 0x0C;
-const WRITE_MULTIPLE_COILS      = 0x0F;
-const WRITE_MULTIPLE_REGISTERS  = 0x10;
-const REPORT_SERVER_ID          = 0x11;
-const READ_FILE_RECORD          = 0x14;
-const WRITE_FILE_RECORD         = 0x15;
-const MASK_WRITE_REGISTER       = 0x16;
+const READ_COILS = 0x01;
+const READ_DISCRETE_INPUTS = 0x02;
+const READ_HOLDING_REGISTERS = 0x03;
+const READ_INPUT_REGISTERS = 0x04;
+const WRITE_SINGLE_COIL = 0x05;
+const WRITE_SINGLE_REGISTER = 0x06;
+const READ_EXCEPTION_STATUS = 0x07;
+const GET_COMM_EVENT_COUNTER = 0x0B;
+const GET_COMM_EVENT_LOG = 0x0C;
+const WRITE_MULTIPLE_COILS = 0x0F;
+const WRITE_MULTIPLE_REGISTERS = 0x10;
+const REPORT_SERVER_ID = 0x11;
+const READ_FILE_RECORD = 0x14;
+const WRITE_FILE_RECORD = 0x15;
+const MASK_WRITE_REGISTER = 0x16;
 const READ_WRITE_MULTIPLE_REGISTERS = 0x17;
-const READ_FIFO_QUEUE           = 0x18;
-const ENCAPSULATED_INTERFACE_TRANSPORT  = 0x2B;
+const READ_FIFO_QUEUE = 0x18;
+const ENCAPSULATED_INTERFACE_TRANSPORT = 0x2B;
 
-const MODBUS_DEVICE_ADDRESS     = 1;
+const MODBUS_DEVICE_ADDRESS = 1;
 
 const MAX_READ_TRYCOUNT = 0;
 
+/**
+ * @param {*} buffer
+ * @param {*} deviceAddress 
+ * @param {*} functionCode 
+ */
 
-function makeTwoWordRegisterCommand(deviceAddress, functionCode, firstWord, secondWord, buffer) {
-    buffer[0] = deviceAddress;
-    buffer[1] = functionCode;
-    buffer[2] = (firstWord >>> 8) & 0xFF;
-    buffer[3] = firstWord & 0xFF;
-    buffer[4] = (secondWord >>> 8) & 0xFF;
-    buffer[5] = secondWord & 0xFF;
+function makeCommandBuffer(buffer, deviceAddress, functionCode) {
+    var i;
+    var bufferPtr = 0;
+    buffer[bufferPtr++] = deviceAddress;
+    buffer[bufferPtr++] = functionCode;
+    /** getting arguments from argument list.  */
+    for (i = 3; i < arguments.length; i++) {
+        buffer[bufferPtr++] = (arguments[i] >>> 8) & 0xFF;
+        buffer[bufferPtr++] = arguments[i] & 0xFF;
+//        console.log("arguments[" + i + "] = ", arguments[i]);
+    }
+    return bufferPtr;
 }
 
+function storeCRCOnBufferTail(buffer, nextData) {
+    var CRC = ModRTU_CRC(buffer, nextData);
+    outBuffer[nextData++] = CRC & 0xFF;
+    outBuffer[nextData++] = (CRC >>> 8) & 0xFF;
+    return nextData;
+}
+
+/**
+ * 
+ * @param {*} deviceAddress 
+ * @param {*} regAddress 
+ * @param {*} value 
+ */
 function assemblyWriteRegisterCommand(deviceAddress, regAddress, value) {
-/*    outBuffer[0] = deviceAddress;
-    outBuffer[1] = WRITE_SINGLE_REGISTER;
-    outBuffer[2] = (regAddress >>> 8) & 0xFF;
-    outBuffer[3] = regAddress & 0xFF;
-    outBuffer[4] = (value >>> 8) & 0xFF;
-    outBuffer[5] = value & 0xFF;*/
-    makeTwoWordRegisterCommand(deviceAddress, WRITE_SINGLE_REGISTER, regAddress, value, outBuffer)
-    var CRC = ModRTU_CRC(outBuffer, 6);
-    outBuffer[6] = CRC & 0xFF;
-    outBuffer[7] = (CRC >>> 8) & 0xFF;
+    nextData = makeCommandBuffer(outBuffer, deviceAddress, WRITE_SINGLE_REGISTER, regAddress, value);
+    storeCRCOnBufferTail(outBuffer, nextData);
 }
 
-function assemblyReadHoldingRegistersCommand(deviceAddress, startAddress) {
-    outBuffer[0] = deviceAddress;
-    outBuffer[1] = READ_HOLDING_REGISTERS;
-    outBuffer[2] = (startAddress >>> 8) & 0xFF;
-    outBuffer[3] = startAddress & 0xFF;
-    outBuffer[4] = 0;
-    outBuffer[5] = 1;
-    //  outBuffer[4] = (regQuantity >>> 8) & 0xFF;
-    //  outBuffer[5] = regQuantity & 0xFF;
-    var CRC = ModRTU_CRC(outBuffer, 6);
-    outBuffer[6] = CRC & 0xFF;
-    outBuffer[7] = (CRC >>> 8) & 0xFF;
-
+function assemblyReadHoldingRegistersCommand(deviceAddress, startAddress, quantityRegisters) {
+    nextData = makeCommandBuffer(outBuffer, deviceAddress, READ_HOLDING_REGISTERS, startAddress, quantityRegisters);
+    storeCRCOnBufferTail(outBuffer, nextData);
 }
 
 // Open errors will be emitted as an error event
@@ -110,36 +120,38 @@ port.on('error', function (err) {
 function processData(readBuffer) {
     console.log("READBUFFER = ", readBuffer);
     if (readBuffer.length > 2) {
-      CRCLength = readBuffer.length - 2;
-      var CRC = ModRTU_CRC(readBuffer, CRCLength);
-  
-      if (((CRC & 0xFF) == readBuffer[readBuffer.length - 2]) && ((CRC >>> 8) & 0xFF) == readBuffer[readBuffer.length - 1]) {
-        //      console.log('CRC LO = ', (CRC & 0xFF))
-        //      console.log("CRC HI = ", (CRC >>> 8) & 0xFF);
-        console.log("Reading data\'s CRC OK")
-        return true;
-      } else {
-        console.log("Bad CRC of Reading data\'s")
-        return false;
-      }
-  
+        CRCLength = readBuffer.length - 2;
+        var CRC = ModRTU_CRC(readBuffer, CRCLength);
+
+        if (((CRC & 0xFF) == readBuffer[readBuffer.length - 2]) && ((CRC >>> 8) & 0xFF) == readBuffer[readBuffer.length - 1]) {
+            //      console.log('CRC LO = ', (CRC & 0xFF))
+            //      console.log("CRC HI = ", (CRC >>> 8) & 0xFF);
+            console.log("Reading data\'s CRC OK")
+            return true;
+        } else {
+            console.log("Bad CRC of Reading data\'s")
+            return false;
+        }
+
     } else {
-      console.log("readBuffer.length <= 2)")
-      return false;
+        console.log("readBuffer.length <= 2)")
+        return false;
     }
     return false;
     //  resolve(data);
-  }
+}
 
 /**App.get get the MODBUS register data. */
 
-app.get('/test/holding/get/:address', function (req, res, next) {
+app.get('/test/holding/get/:address/:quantity', function (req, res, next) {
 
     p = new Promise((resolve, reject) => {
-        assemblyReadHoldingRegistersCommand(MODBUS_DEVICE_ADDRESS, req.params.address);
+        assemblyReadHoldingRegistersCommand(MODBUS_DEVICE_ADDRESS, req.params.address, req.params.quantity);
         console.log("WRITEBUFFER = ", outBuffer);
+
         /**Write MODBUS outbuffer to serial. */
         port.write(outBuffer);
+
         /**Read the answer.  */
         port.once('data', (data) => {
             /**Process the reading datas. */
@@ -159,6 +171,8 @@ app.get('/test/holding/get/:address', function (req, res, next) {
 
         console.log("READBUFFER AWAIT = ", data);
         console.log('address', req.params.address);
+
+//        for (int i = 0; i < data)
 
         var valHi = data[3 + (0 * 2)];
         var valLow = data[4 + (0 * 2)];
@@ -192,3 +206,5 @@ app.listen(PORT, function (err) {
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 });
+
+//assemblyReadHoldingRegistersCommand(1, 3, 2);
